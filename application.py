f0a75300c49a4976dfe42d8b3b441c753bd7c419
SharpVisionSoft <dong.wang@sharpvisionsoft.com>
2019-12-19 10:29:57 +0800

    a

---
+++ application.py
@@ -0,0 +1,31 @@
+from flask import Flask
+# from urllib.request import Request, urlopen
+# from ssl import _create_unverified_context
+import requests
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World123!!!"
+
+# @app.route("/register")
+# def register():
+#     para_data = {
+#         "profile": {
+#             "firstName": "application201111",
+#             "lastName": "okta111",
+#             "email": "atul.gautam111@cardinalhealth.com",
+#             "login": "application201.okta111@cah.com"
+#         },
+#         "groupIds": ["o-cordis-customers"]
+#     }
+#     headers = {'Authorization': 'SSWS 00Vyd70nQIOJlxKD2ToO3lVUbYRgvuXMxGuDfkVRwO'}
+#     try:
+#         #context = _create_unverified_context()
+#         with urlopen(
+#             Request("https://cardinalb2b.oktapreview.com/api/v1/users?activate=false", headers=headers, method="POST"),
+#             data=para_data
+#         ) as response:
+#             return response.read()
+#     except Exception as e:
+#         return "error: %s" % e
